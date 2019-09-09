080dc362a3ece8b419b6f2cc42b0dc5cbecf4202
Qizhe <qizhe.cai@gmail.com>
2019-09-08 21:19:03 -0400

    change to server

--- server.c
+++ server.c
@@ -3,19 +3,23 @@
 #include <stdio.h> 
 #include <sys/socket.h> 
 #include <stdlib.h> 
+#include <arpa/inet.h> 
 #include <netinet/in.h> 
 #include <string.h> 
 #include <pthread.h>
-#define PORT 8080 
+#include <limits.h>
 // inet_pton ()
 #define TOTAL_FLOW 30000
-void* receive_flow(void* arg) {
-	int new_socket = *((int *) arg);
-	char buffer[1024] = {0}; 
+#define THREADSTACK (PTHREAD_STACK_MIN + 16384)
 
+void* receive_flow(void* arg) {
+	int new_socket = (int) arg;
+	char buffer[1460] = {0}; 
+	// printf("new connection\n");
 	while(1) {
-		int valread = read(new_socket, buffer, 1024);
+		int valread = read(new_socket, buffer, 1460);
 		if(buffer[valread - 1] == 'd') {
+			// printf("get d: val read:%d\n", valread);
 			char* end_str = "d";
 			valread = send(new_socket , end_str , 1 , 0);
 			while(valread != 1) {
@@ -24,18 +28,22 @@ void* receive_flow(void* arg) {
 			break;
 		}
 	}
-    close(new_socket);
+    if(close(new_socket) < 0) {
+    	exit(EXIT_FAILURE);
+    }
     pthread_exit(NULL);
 
 }
 int main(int argc, char const *argv[]) 
 { 
-	int server_fd, new_socket, valread; 
+	int server_fd, valread; 
 	struct sockaddr_in address; 
 	int opt = 1; 
 	int addrlen = sizeof(address); 
 	char *hello = "Hello from server"; 
-	
+	const char *server_addr = argv[1];
+	int server_port = atoi(argv[2]);
+
 	// Creating socket file descriptor 
 	if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) 
 	{ 
@@ -50,9 +58,14 @@ int main(int argc, char const *argv[])
 		perror("setsockopt"); 
 		exit(EXIT_FAILURE); 
 	} 
+	if(inet_pton(AF_INET, server_addr, &address.sin_addr)<=0) 
+	{ 
+		printf("\nInvalid address/ Address not supported \n"); 
+	} 
 	address.sin_family = AF_INET; 
-	address.sin_addr.s_addr = INADDR_ANY; 
-	address.sin_port = htons( PORT ); 
+	// address.sin_addr.s_addr = INADDR_ANY;
+
+	address.sin_port = htons( server_port ); 
 	
 	// Forcefully attaching socket to the port 8080 
 	if (bind(server_fd, (struct sockaddr *)&address, 
@@ -67,15 +80,20 @@ int main(int argc, char const *argv[])
 		exit(EXIT_FAILURE); 
 	} 
     pthread_t threads[TOTAL_FLOW];
+    pthread_attr_t  attrs;
+    pthread_attr_init(&attrs);
+    pthread_attr_setstacksize(&attrs, THREADSTACK);
     int i = 0;
 	while (1) 
 	{ 
-		new_socket = accept(server_fd, (struct sockaddr *)&address, 
+		int new_socket = accept(server_fd, (struct sockaddr *)&address, 
 					(socklen_t*)&addrlen);
-    	pthread_create(&threads[i], NULL, receive_flow, &new_socket);
+    	pthread_create(&threads[i], &attrs, receive_flow, (void*)new_socket);
     	i++;
 
 	} 
+    pthread_attr_destroy(&attrs);
+
 	pthread_exit(NULL);
 } 
 
